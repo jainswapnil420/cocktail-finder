3549ecc29430cb97ee8af3715e8c16b0b646a175
swapnil <swapnil.jain.ap@nielsen.com>
2020-10-21 18:49:48 +0530

    added comments on functions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { LoadcocktailService } from './service/loadcocktail.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import {FilterType} from './service/filterType';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit, OnDestroy{
   filterType = FilterType;
   selectedFilterType: FilterType;
   cockTailList: any = [];
@@ -17,35 +18,49 @@ export class AppComponent implements OnInit{
   searchByCategoryValue = this.categories[0];
   searchByIngredientValue = this.ingredients[0];
   isAscending: boolean;
+  searchByNameSubscriptions: Subscription;
+  searchByCategorySubscriptions: Subscription;
+  searchByIngredientSubscriptions: Subscription;
 
   constructor(private loadCocktailService: LoadcocktailService){}
+  // Initialize value for first launch
   ngOnInit(): void{
     this.selectedFilterType = FilterType.name;
-    this.searchByNameValue = 'margarita';
+    this.searchByNameValue = 'Margarita';
     this.searchByName();
    }
+   // Destroy subscription to release memory
+   ngOnDestroy(): void {
+     if (this.searchByNameSubscriptions) {this.searchByNameSubscriptions.unsubscribe(); }
+     if (this.searchByCategorySubscriptions) {this.searchByCategorySubscriptions.unsubscribe(); }
+     if (this.searchByIngredientSubscriptions) {this.searchByIngredientSubscriptions.unsubscribe(); }
+   }
+   // On change of filter type update the data and filter dropdowns
    onChangeFilter(event): void{
     this.selectedFilterType = event.target.value;
-    if (this.selectedFilterType === FilterType.name) { this.searchByName(); }
+    if (this.selectedFilterType === FilterType.name) {  this.searchByName(); }
     if (this.selectedFilterType === FilterType.category) { this.searchByCategory(); }
     if (this.selectedFilterType === FilterType.ingredient) { this.searchByIngredient(); }
   }
+  // Get List of cocktails based on selected ingredient
   searchByIngredient(): void{
-  this.loadCocktailService.loadCockTailByIngredients(this.searchByIngredientValue).subscribe(resp => {
-    this.cockTailList = resp;
-  });
+    this.searchByIngredientSubscriptions = this.loadCocktailService
+                                                  .loadByIngredients(this.searchByIngredientValue)
+                                                  .subscribe(resp => this.cockTailList = resp);
   }
+  // Get list of cocktails based on selected category
   searchByCategory(): void{
-  this.loadCocktailService.loadCockTailByCategory(this.searchByCategoryValue).subscribe(resp => {
-    this.cockTailList = resp;
-  });
+  this.searchByCategorySubscriptions = this.loadCocktailService
+                                          .loadByCategory(this.searchByCategoryValue)
+                                          .subscribe(resp => this.cockTailList = resp);
   }
+  // Get list of cocktails based on selected name
   searchByName(): void{
-    console.log(this.searchByNameValue)
-    this.loadCocktailService.loadCockTailByName(this.searchByNameValue).subscribe(resp => {
-      this.cockTailList = resp;
-    });
+   this.searchByNameSubscriptions = this.loadCocktailService
+                                            .loadByName(this.searchByNameValue)
+                                            .subscribe(resp => this.cockTailList = resp);
   }
+  // Returns the sorted data
   sortData(): void{
     this.cockTailList.sort((a, b) => ((a.strDrink > b.strDrink) && this.isAscending) ? 1 : -1);
     this.isAscending = !this.isAscending;
--- src/app/service/loadcocktail.service.ts
+++ src/app/service/loadcocktail.service.ts
@@ -2,26 +2,32 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { environment } from './../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoadcocktailService {
-
   constructor(private http: HttpClient) { }
 
-  loadCockTailByName(name: string): Observable<any>{
+  // Call cocktail service to get list of cocktails based on selected name
+  // Using pipe to return list of drinks
+  loadByName(name: string): Observable<any>{
    // tslint:disable-next-line:no-string-literal
-   return this.http.get('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=' + name).pipe(map(resp => resp['drinks']));
+   return this.http.get(`${environment.apiUrl}search.php?s=${name}`).pipe(map(resp => resp['drinks']));
   }
 
-  loadCockTailByCategory(name: string): Observable<any>{
+  // Call cocktail service to get list of cocktails based on selected category
+  // Using pipe to return list of drinks
+  loadByCategory(name: string): Observable<any>{
     // tslint:disable-next-line:no-string-literal
-    return this.http.get('https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=' + name).pipe(map(resp => resp['drinks']));
-   }
-   loadCockTailByIngredients(name: string): Observable<any>{
-    // tslint:disable-next-line:no-string-literal
-    return this.http.get('https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=' + name).pipe(map(resp => resp['drinks']));
+    return this.http.get(`${environment.apiUrl}filter.php?c=${name}`).pipe(map(resp => resp['drinks']));
    }
 
+  // Call cocktail service to get list of cocktails based on selected ingredients
+  // Using pipe to return list of drinks
+   loadByIngredients(name: string): Observable<any>{
+    // tslint:disable-next-line:no-string-literal
+    return this.http.get(`${environment.apiUrl}filter.php?i=${name}`).pipe(map(resp => resp['drinks']));
+   }
   }
